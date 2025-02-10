2af412f86fe69f3c1ca43723a2c97b19b26afe11
polina <pbarmakova@yandex.ru>
2025-02-10 19:40:36 +0300

    step 6

--- src/Parsing.php
+++ src/Parsing.php
@@ -1,9 +1,49 @@
 <?php
 
 namespace Differ\Parsing;
+use Symfony\Component\Yaml\Yaml;
+use Symfony\Component\Yaml\Exception\ParseException;
+
+function is_yaml_file($file) {
+    $extension = pathinfo($file, PATHINFO_EXTENSION);
+    if ($extension !== 'yaml' && $extension !== 'yml') {
+        return false;
+    }
+    try {
+        $data = Yaml::parseFile($file);
+        // Если парсинг прошел успешно, то файл является YAML
+        return true;
+    } catch (ParseException $e) {
+        // Ошибка парсинга, значит файл не является корректным YAML
+        return false;
+    }
+}
+
+function is_json_file($file) {
+    // Проверка расширения файла (не обязательно, но полезно)
+    $extension = pathinfo($file, PATHINFO_EXTENSION);
+    if ($extension !== 'json') {
+        return false;
+    }
+
+    // Чтение содержимого файла
+    $content = file_get_contents($file);
+
+    // Попытка декодировать содержимое как JSON
+    json_decode($content);
+
+    // Проверка на ошибки декодирования
+    return json_last_error() === JSON_ERROR_NONE;
+}
+
 
 function runParsing($file)
 {
-    $fileContents = file_get_contents($file);
-    return (json_decode($fileContents, true));
+    if (is_yaml_file($file)) {
+        return Yaml::parseFile($file);
+    }
+    if (is_json_file($file)) {
+        $fileContents = file_get_contents($file);
+        return (json_decode($fileContents, true));
+    }
 }
--- tests/DifferTest.php
+++ tests/DifferTest.php
@@ -23,7 +23,7 @@ EOL;
         $this->assertEquals($expected, $diff);
     }
 
-    public function testDifferentKeys()
+    public function testDifferentKeysJsonFiles()
     {
         $file1 = 'tests/fixtures/file1.json';
         $file2 = 'tests/fixtures/file4.json';
@@ -42,10 +42,65 @@ EOL;
         $this->assertEquals($expected, $diff);
     }
 
-    public function testDifferentValues()
+    public function testDifferentValuesJsonFiles()
     {
         $file1 = 'tests/fixtures/file1.json';
         $file2 = 'tests/fixtures/file5.json';
+
+        $expected = <<<EOL
+  follow: 
+  host: hexlet.io
+  proxy: 123.234.53.22
+- timeout: 50
++ timeout: 10
+
+EOL;
+
+        $diff = genDiff($file1, $file2);
+
+        $this->assertEquals($expected, $diff);
+    }
+
+    public function testIdenticalYamlFiles()
+    {
+        $file1 = 'tests/fixtures/file1.yaml';
+        $file2 = 'tests/fixtures/file3.yaml';
+        $expected = <<<EOL
+  follow: 
+  host: hexlet.io
+  proxy: 123.234.53.22
+  timeout: 50
+
+EOL;
+
+        $diff = genDiff($file1, $file2);
+
+        $this->assertEquals($expected, $diff);
+    }
+
+    public function testDifferentKeysYamlFiles()
+    {
+        $file1 = 'tests/fixtures/file1.yaml';
+        $file2 = 'tests/fixtures/file4.yaml';
+
+        $expected = <<<EOL
+  follow: 
+- host: hexlet.io
++ name: alice
+  proxy: 123.234.53.22
+  timeout: 50
+
+EOL;
+
+        $diff = genDiff($file1, $file2);
+
+        $this->assertEquals($expected, $diff);
+    }
+
+    public function testDifferentValuesYamlFiles()
+    {
+        $file1 = 'tests/fixtures/file1.yaml';
+        $file2 = 'tests/fixtures/file5.yaml';
 
         $expected = <<<EOL
   follow: 
